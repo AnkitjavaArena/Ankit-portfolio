38c0c142792bb5cbe500f34d0622fa11a29be3e9
Ankit Tripathi <ankit.scrps@gmail.com>
2024-12-28 12:25:53 +0530

    fixes social-media links and resume download option

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -65,9 +65,12 @@ const Home = () => {
             </div>
             <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
               <div className="mt-3 sm:mt-0 cursor-pointer w-1/2">
-                <Link className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-500 hover:bg-blue-200 md:py-4 md:text-lg md:px-10">
-                  Resume
-                </Link>
+                <a 
+                 href="/Ankit_Resume_Java.pdf" // Update this path as per your file location
+                 download="Ankit_Resume_Java.pdf"
+                 className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-500 hover:bg-blue-200 md:py-4 md:text-lg md:px-10">
+                  Download Resume
+                </a>
               </div>
             </div>
           </div>
